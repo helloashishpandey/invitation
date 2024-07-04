bad75e0c9a9789668378ac1031dc92f15670b640
Ashish Pandey <63122223+helloashishpandey@users.noreply.github.com>
2024-07-04 18:10:28 +0530

    Add files via upload

---
+++ src/Card.jsx
@@ -0,0 +1,29 @@
+export default () => (
+    <div className="birthdayCardContainer">
+      <div className="birthdayCard">
+        <div className="cardFront">
+          <h3 className="happy">You are Invited!</h3>
+          <div className="balloons">
+            <div className="balloonOne" />
+            <div className="balloonTwo" />
+            <div className="balloonThree" />
+            <div className="balloonFour" />
+          </div>
+        </div>
+        <div className="cardInside">
+          <h3 className="back">Our Baby's First Birthday!</h3>
+          <p>Dear Friend,</p>
+          <p>
+            We are delighted to invite you to celebrate our baby's first birthday!
+            Join us for a day filled with love and laughter.
+          </p>
+          <p className="name">Mukesh Mandal</p>
+          <div className="cardDetails">
+          <p><b style={{ fontFamily: "Arial", fontSize: "10px" }}>Date: 08/July/2024</b></p>
+          <p><b style={{ fontFamily: "Arial", fontSize: "10px" }}>Venue: Guru Kripa Garden Rakdi, Sodala, Jaipur</b></p>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+  
